0c278707a87863858c3c1092ecf6722536792160
Manuel Burghard <manuel.burghard@googlemail.com>
2011-12-15 18:02:59 +0100

    Basic setup.

---
+++ Source/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ninja-Projekt
+//
+//  Created by Manuel Burghard on 15.12.11.
+//  Copyright (c) 2011 ESP 2011/12/13. All rights reserved.
+//
+
+#include <iostream>
+#include <irrlicht.h>
+
+int main() {
+    return 0;
+}
