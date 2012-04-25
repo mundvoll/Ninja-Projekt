107e04e716dd588c66a4e0fb7936343921ad4f8f
Manuel Burghard <manuel.burghard@googlemail.com>
2012-04-25 20:22:37 +0200

    Global definitions.

---
+++ Source/Definitions.h
@@ -0,0 +1,16 @@
+//
+//  Definitions.h
+//  Ninja-Projekt
+//
+//  Created by Manuel Burghard on 21.03.12.
+//  Copyright (c) 2012 ESP 2011/12/13. All rights reserved.
+//
+
+#ifndef Ninja_Projekt_Definitions_h
+#define Ninja_Projekt_Definitions_h
+
+#define DWARF_ID 1
+
+#define printStringw(str) printf("%ls\n\n", str.c_str());
+
+#endif
