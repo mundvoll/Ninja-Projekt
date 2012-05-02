3864df19894aa72aa9ebf91d1367c6742d92a223
Manuel Burghard <manuel.burghard@googlemail.com>
2012-05-02 14:55:04 +0200

    removed some accidentally pushed files

--- Source/MacOSX/CCharacter.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  CCharacter.cpp
-//  Ninja-Projekt
-//
-//  Created by Manuel Burghard on 04.02.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
--- Source/MacOSX/ESPModelImporter.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ESPModelImporter.cpp
-//  Ninja-Projekt
-//
-//  Created by Manuel Burghard on 12.03.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
